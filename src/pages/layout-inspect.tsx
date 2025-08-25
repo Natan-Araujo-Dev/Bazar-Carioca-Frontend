c1fe39dc5c514b734132ca24a6596596b9d53318
Natan Araujo <natan.araujo.dev@gmail.com>
2025-08-25 19:09:59 -0300

    - Tentativa de fix

--- src/pages/layout-inspect.tsx
+++ src/pages/layout-inspect.tsx
@@ -1,20 +1,23 @@
 import { Outlet } from "react-router";
+import { SearchProvider } from "../contexts/SearchContext";
 import Context from "../core-components/context";
 import Footer from "../core-components/footer";
 import Header from "../core-components/header";
 
 export default function LayoutInspect() {
 	return (
-		<div className="flex flex-col min-h-screen">
-			<Header />
+		<SearchProvider>
+			<div className="flex flex-col min-h-screen">
+				<Header />
 
-			<Context />
+				<Context />
 
-			<main className="flex flex-1 items-center justify-center py-15">
-				<Outlet />
-			</main>
+				<main className="flex flex-1 mx-4 items-center justify-center py-15">
+					<Outlet />
+				</main>
 
-			<Footer />
-		</div>
+				<Footer />
+			</div>
+		</SearchProvider>
 	);
 }
