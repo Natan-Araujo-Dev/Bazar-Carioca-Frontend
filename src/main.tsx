79cbb71021a7ce3b42679da622d34bf2a3e368aa
Natan Araujo <natan.araujo.dev@gmail.com>
2025-08-25 19:17:21 -0300

    - Outra tentativa de fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { SearchProvider } from "./contexts/SearchContext";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<SearchProvider>
-			<App />
-		</SearchProvider>
+		<App />
 	</StrictMode>,
 );
--- src/pages/layout-main.tsx
+++ src/pages/layout-main.tsx
@@ -1,17 +1,20 @@
 import { Outlet } from "react-router";
+import { SearchProvider } from "../contexts/SearchContext";
 import Footer from "../core-components/footer";
 import Header from "../core-components/header";
 
 export default function LayoutMain() {
 	return (
-		<div className="flex flex-col min-h-screen gap-20">
-			<Header />
+		<SearchProvider>
+			<div className="flex flex-col min-h-screen gap-20">
+				<Header />
 
-			<main className="flex flex-1 mx-4 items-center justify-center">
-				<Outlet />
-			</main>
+				<main className="flex flex-1 mx-4 items-center justify-center">
+					<Outlet />
+				</main>
 
-			<Footer />
-		</div>
+				<Footer />
+			</div>
+		</SearchProvider>
 	);
 }
