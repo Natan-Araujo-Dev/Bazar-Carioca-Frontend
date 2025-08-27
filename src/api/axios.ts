53510437a23038a4b10b40efe019745d63b64276
Natan Araujo <natan.araujo.dev@gmail.com>
2025-08-26 22:46:38 -0300

    - fix pro vercel

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,10 +5,10 @@ const api = axios.create({
 	// baseURL: "http://localhost:5000/bazar-carioca",
 
 	// backend online:
-	baseURL: "http://15.228.192.242:5000/Bazar-Carioca",
+	//baseURL: "http://15.228.192.242:5000/Bazar-Carioca",
 
 	// backend pelo vercel
-	// baseURL: "/api/Bazar-Carioca",
+	baseURL: "/api/Bazar-Carioca",
 
 	headers: { "Content-Type": "application/json" },
 	timeout: 10_000,
