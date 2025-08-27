418ab8189aa1f17ccb32ffa8bcbe827953a4d0e7
Natan Araujo <natan.araujo.dev@gmail.com>
2025-08-26 21:38:59 -0300

    - Fix do context - Botões agora expandem com animação - Tipos de produtos informam quando estão vazios - Descrição e estoque vazio(a) agora é informado(a) que está vazio(a)

--- src/pages/layout-inspect.tsx
+++ src/pages/layout-inspect.tsx
@@ -1,20 +1,17 @@
 import { Outlet } from "react-router";
-import { SearchProvider } from "../contexts/SearchContext";
 import Context from "../core-components/context";
 import Footer from "../core-components/footer";
 import Header from "../core-components/header";
 
 export default function LayoutInspect() {
 	return (
-		<SearchProvider>
-			<div className="flex flex-col min-h-screen">
-				<Header />
-				<Context />
-				<main className="flex flex-1 mx-4 items-center justify-center py-15">
-					<Outlet />
-				</main>
-				<Footer />
-			</div>
-		</SearchProvider>
+		<div className="flex flex-col min-h-screen">
+			<Header />
+			<Context />
+			<main className="flex flex-1 mx-4 items-center justify-center py-15">
+				<Outlet />
+			</main>
+			<Footer />
+		</div>
 	);
 }
--- src/pages/layout-main.tsx
+++ src/pages/layout-main.tsx
@@ -1,20 +1,17 @@
 import { Outlet } from "react-router";
-import { SearchProvider } from "../contexts/SearchContext";
 import Footer from "../core-components/footer";
 import Header from "../core-components/header";
 
 export default function LayoutMain() {
 	return (
-		<SearchProvider>
-			<div className="flex flex-col min-h-screen gap-20">
-				<Header />
+		<div className="flex flex-col min-h-screen gap-20">
+			<Header />
 
-				<main className="flex flex-1 mx-4 items-center justify-center">
-					<Outlet />
-				</main>
+			<main className="flex flex-1 mx-4 items-center justify-center">
+				<Outlet />
+			</main>
 
-				<Footer />
-			</div>
-		</SearchProvider>
+			<Footer />
+		</div>
 	);
 }
